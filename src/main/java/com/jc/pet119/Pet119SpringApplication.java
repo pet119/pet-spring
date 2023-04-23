1f480f649581364daed9d5610f894fcc00641ac4
hoon25 <cchoon95@hanmail.net>
2023-04-23 23:13:05 +0900

    Add spring project

---
+++ src/main/java/com/jc/pet119/Pet119SpringApplication.java
@@ -0,0 +1,13 @@
+package com.jc.pet119;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pet119SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pet119SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jc/pet119/Pet119SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.pet119;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pet119SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
